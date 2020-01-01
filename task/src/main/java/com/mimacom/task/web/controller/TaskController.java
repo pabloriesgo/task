851839b0d9d90e84dd21de8a7ff3f3280f95173a
PabloRiesgo <pabloriesgo@gmail.com>
2020-01-01 23:14:35 +0100

    Uso de swagger y javadoc del api del controller

--- task/src/main/java/com/mimacom/task/web/controller/TaskController.java
+++ task/src/main/java/com/mimacom/task/web/controller/TaskController.java
@@ -18,6 +18,8 @@ import com.mimacom.task.exception.TaskNotFoundException;
 import com.mimacom.task.repository.TaskRepository;
 
 /**
+ * Controller to handle requests to task API
+ * 
  * @author priesgo
  *
  */
@@ -31,31 +33,54 @@ public class TaskController {
 		this.taskRepository = taskRepository;
 	}
 
-	//TODO --> Sacar negocio a jar y ver como hacer esto multimódulo
-	
+	// TODO --> Sacar negocio a jar y ver como hacer esto multimódulo
+
 	// TODO --> JAVADOC de todo --> REVISAR SWAGGER
 
 	// TODO --> HAcaer mensajes de error HTTP para respuestas de error guays
 
-	// TODO --> Estudiar convenio URL's
-
 	// TODO --> Nombre único
 
+	/**
+	 * List all existing tasks
+	 * 
+	 * @return list of all existing tasks embedded in objects of type
+	 *         <code>Task</code>
+	 */
 	@GetMapping("/tasks")
 	public List<Task> all() {
 		return taskRepository.findAll();
 	}
 
+	/**
+	 * Retrieve a task from its identifier
+	 * 
+	 * @param id identifier of the task to retrieve
+	 * @return task found or throws a <code>TaskNotFoundException</code> if it does
+	 *         not exist
+	 */
 	@GetMapping("/tasks/{id}")
 	public Task one(@PathVariable Integer id) {
 		return taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
 	}
 
+	/**
+	 * Create a new task
+	 * 
+	 * @param task container of task data
+	 * @return data of the created task
+	 */
 	@PostMapping("/tasks")
 	public Task newTask(@RequestBody Task task) {
 		return taskRepository.save(task);
 	}
 
+	/**
+	 * Mark a task as finished
+	 * 
+	 * @param id identifier of the task to mark
+	 * @return data of the modified task
+	 */
 	@PostMapping("/tasks/{id}/finish")
 	public Task finishTask(@PathVariable Integer id) {
 		Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
@@ -63,12 +88,24 @@ public class TaskController {
 		return taskRepository.save(task);
 	}
 
+	/**
+	 * Remove a task
+	 * 
+	 * @param id identifier of the task to remove
+	 */
 	@DeleteMapping("/tasks/{id}")
 	public void deleteTask(@PathVariable Integer id) {
 		// TODO --> ver si puedo manejar ERROR!
 		taskRepository.deleteById(id);
 	}
 
+	/**
+	 * Update a task. If it does not exist, create a new task with the given data.
+	 * 
+	 * @param newTask data to modify of the task
+	 * @param id identifier of the task to modify
+	 * @return data updated or created
+	 */
 	@PutMapping("/tasks/{id}")
 	public Task updateTask(@RequestBody Task newTask, @PathVariable Integer id) {
 		return taskRepository.findById(id).map(task -> {
